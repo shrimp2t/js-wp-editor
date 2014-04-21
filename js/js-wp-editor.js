372b0e1a02b92ec1507eeadaecc3d9de6fa81696
shrimp2t <shrimp2t@gmail.com>
2014-04-21 22:42:21 +0700

    Try with this code
    I think your code still problems !.
    
    Thank for great working :)

--- js/js-wp-editor.js
+++ js/js-wp-editor.js
@@ -2,7 +2,7 @@
  *	JavaScript Wordpress editor
  *	Author: 		Ante Primorac
  *	Author URI: 	http://anteprimorac.from.hr
- *	Version: 		1.1
+ *	Version: 		1.0
  *	License:
  *		Copyright (c) 2013 Ante Primorac
  *		Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -29,185 +29,131 @@
  *			$('textarea').wp_editor( options );
  */
 
+
 ;(function( $, window ) {
-	$.fn.wp_editor = function( options ) {
+    $.fn.wp_editor = function( options ) {
+        if( !$(this).is('textarea') || typeof window.tinyMCEPreInit == 'undefined' || typeof QTags == 'undefined' ) return this;
 
-		if( !$(this).is('textarea') )
-			console.warn('Element must be a textarea');
+        var default_options = {
+            'wp_root': location.origin,
+            'mode': 'html',
+            'mceInit': {
+                "mode":"textareas",
+                "width":"100%",
+                "theme": window.tinyMCEPreInit.mceInit.content.theme,
+                "skin": window.tinyMCEPreInit.mceInit.content.skin,
+                "language": window.tinyMCEPreInit.mceInit.content.lang,
 
-		if( typeof tinyMCEPreInit == 'undefined' || typeof QTags == 'undefined' || typeof ap_vars == 'undefined' )
-			console.warn('js_wp_editor( $settings ); must be loaded');
+                "formats": window.tinyMCEPreInit.mceInit.formats,
+                "relative_urls":false,
+                "remove_script_host":false,
+                "convert_urls":false,
+                "remove_linebreaks":false,
+                browser_spellcheck: true,
+                "fix_list_elements":true,
+                entity_encoding: "raw",
+                "keep_styles":true,
+                "entities":window.tinyMCEPreInit.mceInit.content.entities,
+                "accessibility_focus":true,
+                "media_strict":false,
+                "paste_remove_styles":false,
+                "paste_remove_spans":false,
+                "paste_text_use_dialog":true,
+                "webkit_fake_resize":false,
+                "preview_styles": window.tinyMCEPreInit.mceInit.content.preview_styles,
+                "schema":"html5",
+                 wpeditimage_disable_captions:  window.tinyMCEPreInit.mceInit.content.wpeditimage_disable_captions,
+                 wpeditimage_html5_captions:  window.tinyMCEPreInit.mceInit.content.wpeditimage_html5_captions,
+                "wp_fullscreen_content_css": tinyMCEPreInit.base + "/plugins/wpfullscreen/css/wp-fullscreen.css",
+                external_plugins: window.tinyMCEPreInit.mceInit.content.external_plugins,
+                "plugins": window.tinyMCEPreInit.mceInit.content.plugins,
+                "content_css": window.tinyMCEPreInit.mceInit.content.content_css,
+                "elements":"ap[id]",
+                 menubar: false,
+                "wpautop":true,
+                indent: false,
+                toolbar1: window.tinyMCEPreInit.mceInit.content.toolbar1 ,
+                toolbar2: window.tinyMCEPreInit.mceInit.content.toolbar2 ,
+                toolbar3: window.tinyMCEPreInit.mceInit.content.toolbar3,
+                toolbar4: window.tinyMCEPreInit.mceInit.content.toolbar4,
+                "tabfocus_elements": window.tinyMCEPreInit.mceInit.tabfocus_elements,
+                "body_class":"ap[id]",
+                "theme_advanced_resizing_use_cookie": false
+            }
+        }, id_regexp = new RegExp('ap\\[id\\]', 'g'), wp_root_regexp = new RegExp('ap\\[wp\\_root\\]', 'g');
 
-		if( !$(this).is('textarea') || typeof tinyMCEPreInit == 'undefined' || typeof QTags == 'undefined' || typeof ap_vars == 'undefined' )
-			return this;
-		
-		var default_options = {
-			'mode': 'html',
-			'mceInit' : {
-				"theme": "modern",
-				"skin": "lightgray",
-				"language": "en",
-				"formats": {
-					"alignleft": [
-						{
-							"selector": "p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li",
-							"styles": {"textAlign":"left"},
-							"deep": false,
-							"remove": "none"
-						},
-						{
-							"selector": "img,table,dl.wp-caption",
-							"classes": ["alignleft"],
-							"deep":false,
-							"remove":"none"
-						}
-					],
-					"aligncenter": [
-						{
-							"selector": "p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li",
-							"styles": {"textAlign":"center"},
-							"deep": false,
-							"remove": "none"
-						},
-						{
-							"selector": "img,table,dl.wp-caption",
-							"classes": ["aligncenter"],
-							"deep": false,
-							"remove": "none"
-						}
-					],
-					"alignright": [
-						{
-							"selector": "p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li",
-							"styles": {"textAlign":"right"},
-							"deep": false,
-							"remove": "none"
-						},
-						{
-							"selector": "img,table,dl.wp-caption",
-							"classes": ["alignright"],
-							"deep": false,
-							"remove": "none"
-						}
-					],
-					"strikethrough": {"inline":"del","deep":true,"split":true}
-				},
-				"relative_urls": false,
-				"remove_script_host": false,
-				"convert_urls": false,
-				"browser_spellcheck": true,
-				"fix_list_elements": true,
-				"entities": "38,amp,60,lt,62,gt",
-				"entity_encoding": "raw",
-				"keep_styles": false,
-				"paste_webkit_styles": "font-weight font-style color",
-				"preview_styles": "font-family font-size font-weight font-style text-decoration text-transform",
-				"wpeditimage_disable_captions": false,
-				"wpeditimage_html5_captions": false,
-				"plugins": "charmap,hr,media,paste,tabfocus,textcolor,fullscreen,wordpress,wpeditimage,wpgallery,wplink,wpdialogs,wpview,image",
-				"content_css": ap_vars.includes_url + "css/dashicons.css?ver=3.9," + ap_vars.includes_url + "js/mediaelement/mediaelementplayer.min.css?ver=3.9," + ap_vars.includes_url + "js/mediaelement/wp-mediaelement.css?ver=3.9," + ap_vars.includes_url + "js/tinymce/skins/wordpress/wp-content.css?ver=3.9",
-				"selector": "#apid",
-				"resize": "vertical",
-				"menubar": false,
-				"wpautop": true,
-				"indent": false,
-				"toolbar1": "bold,italic,strikethrough,bullist,numlist,blockquote,hr,alignleft,aligncenter,alignright,link,unlink,wp_more,spellchecker,fullscreen,wp_adv",
-				"toolbar2": "formatselect,underline,alignjustify,forecolor,pastetext,removeformat,charmap,outdent,indent,undo,redo,wp_help",
-				"toolbar3": "",
-				"toolbar4": "",
-				"tabfocus_elements": ":prev,:next",
-				"body_class": "apid"
-			}
-		}, id_regexp = new RegExp('apid', 'g');
 
-		if(tinyMCEPreInit.mceInit['apid']) {
-			default_options.mceInit = tinyMCEPreInit.mceInit['apid'];
-		}
 
-		var options = $.extend(true, default_options, options);
+      //  var default_options  = window.tinyMCEPreInit,  id_regexp = new RegExp('ap\\[id\\]', 'g'), wp_root_regexp = new RegExp('ap\\[wp\\_root\\]', 'g');;
 
-		return this.each(function() {
+        if(window.tinyMCEPreInit.mceInit['ap[id]'])
+            default_options.mceInit = window.tinyMCEPreInit.mceInit['ap[id]'];
 
-			if( !$(this).is('textarea') )
-				console.warn('Element must be a textarea');
-			else {
-				var current_id = $(this).attr('id');
-				$.each( options.mceInit, function( key, value ) {
-					if( $.type( value ) == 'string' )
-					options.mceInit[key] = value.replace(id_regexp, current_id);
-				} );
-				options.mode = options.mode == 'tmce' ? 'tmce' : 'html';
+        var options = $.extend(true, default_options, options);
+        $.each(options.mceInit, function( key, value ) {
+            if( $.type( value ) == 'string' )
+                options.mceInit[key] = value.replace(wp_root_regexp, options.wp_root);
+        });
+        return this.each(function() {
+            if($(this).is('textarea')) {
+               // console.dir(options);
+                var current_id = $(this).attr('id');
+                options.mceInit.elements = options.mceInit.elements.replace(id_regexp, current_id);
+                options.mceInit.body_class = options.mceInit.body_class.replace(id_regexp, current_id);
+             //   options.mode = options.mode == 'tmce' ? 'tmce' : 'html';
 
-				tinyMCEPreInit.mceInit[current_id] = options.mceInit;
+                window.tinyMCEPreInit.mceInit[current_id] = options.mceInit;
 
-				$(this).addClass('wp-editor-area').show();
-				var self = this;
-				if( $(this).closest('.wp-editor-wrap').length ) {
-					var parent_el = $(this).closest('.wp-editor-wrap').parent();
-					$(this).closest('.wp-editor-wrap').before($(this).clone());
-					$(this).closest('.wp-editor-wrap').remove();
-					self = parent_el.find('textarea[id="' + current_id + '"]');
-				}
+                $(this).addClass('wp-editor-area').show();
+                var self = this;
+                if($(this).closest('.wp-editor-wrap').length) {
+                    var parent_el = $(this).closest('.wp-editor-wrap').parent();
+                    $(this).closest('.wp-editor-wrap').before($(this).clone());
+                    $(this).closest('.wp-editor-wrap').remove();
+                    self = parent_el.find('textarea[id="' + current_id + '"]');
+                }
+                var wrap = $('<div id="wp-' + current_id + '-wrap" class="wp-core-ui wp-editor-wrap ' + options.mode + '-active" />'),
+                    editor_tools = $('<div id="wp-' + current_id + '-editor-tools" class="wp-editor-tools hide-if-no-js" />'),
+                    switch_editor_html = $('<a id="' + current_id + '-html" class="wp-switch-editor switch-html" onclick="switchEditors.switchto(this);">Text</a>'),
+                    switch_editor_tmce = $('<a id="' + current_id + '-tmce" class="wp-switch-editor switch-tmce" onclick="switchEditors.switchto(this);">Visual</a>'),
+                    media_buttons = $('<div id="wp-' + current_id + '-media-buttons" class="wp-media-buttons" />'),
+                    insert_media_button = $('<a href="#" id="insert-media-button" class="button insert-media add_media" data-editor="' + current_id + '" title="Add Media"><span class="wp-media-buttons-icon"></span> Add Media</a>'),
+                    editor_container = $('<div id="wp-' + current_id + '-editor-container" class="wp-editor-container" />'),
+                    content_css = false;
+                    //content_css = Object.prototype.hasOwnProperty.call(window.tinyMCEPreInit.mceInit[current_id], 'content_css') ? window.tinyMCEPreInit.mceInit[current_id]['content_css'].split(',') : false;
 
-				var wrap = $('<div id="wp-' + current_id + '-wrap" class="wp-core-ui wp-editor-wrap ' + options.mode + '-active" />'),
-					editor_tools = $('<div id="wp-' + current_id + '-editor-tools" class="wp-editor-tools hide-if-no-js" />'),
-					editor_tabs = $('<div class="wp-editor-tabs" />'),
-					switch_editor_html = $('<a id="' + current_id + '-html" class="wp-switch-editor switch-html" onclick="switchEditors.switchto(this);">Text</a>'),
-					switch_editor_tmce = $('<a id="' + current_id + '-tmce" class="wp-switch-editor switch-tmce" onclick="switchEditors.switchto(this);">Visual</a>'),
-					media_buttons = $('<div id="wp-' + current_id + '-media-buttons" class="wp-media-buttons" />'),
-					insert_media_button = $('<a href="#" id="insert-media-button" class="button insert-media add_media" data-editor="' + current_id + '" title="Add Media"><span class="wp-media-buttons-icon"></span> Add Media</a>'),
-					editor_container = $('<div id="wp-' + current_id + '-editor-container" class="wp-editor-container" />'),
-					content_css = /*Object.prototype.hasOwnProperty.call(tinyMCEPreInit.mceInit[current_id], 'content_css') ? tinyMCEPreInit.mceInit[current_id]['content_css'].split(',') :*/ false;
+                insert_media_button.appendTo(media_buttons);
+                switch_editor_html.appendTo(editor_tools);
+                switch_editor_tmce.appendTo(editor_tools);
+                media_buttons.appendTo(editor_tools);
 
-				insert_media_button.appendTo(media_buttons);
-				media_buttons.appendTo(editor_tools);
+                editor_tools.appendTo(wrap);
+                editor_container.appendTo(wrap);
 
-				switch_editor_html.appendTo(editor_tabs);
-				switch_editor_tmce.appendTo(editor_tabs);
-				editor_tabs.appendTo(editor_tools);
+                editor_container.append($(self).clone());
 
-				editor_tools.appendTo(wrap);
-				editor_container.appendTo(wrap);
+                if( content_css != false )
+                    $.each( content_css, function() {
+                        $(self).before('<link rel="stylesheet" type="text/css" href="' + this + '">');
+                    } );
+                $(self).before(wrap);
+                $(self).remove();
 
-				editor_container.append($(self).clone().addClass('wp-editor-area'));
+                new QTags(current_id);
+                QTags._buttonsInit();
+                switchEditors.go(current_id, options.mode);
 
-				if( content_css != false )
-					$.each( content_css, function() {
-						if( ! $('link[href="' + this + '"]').length )
-							$(self).before('<link rel="stylesheet" type="text/css" href="' + this + '">');
-					} );
-
-				$(self).before('<link rel="stylesheet" id="editor-buttons-css" href="' + ap_vars.includes_url + 'css/editor.css" type="text/css" media="all">');
-
-				$(self).before(wrap);
-				$(self).remove();
-
-				new QTags(current_id);
-				QTags._buttonsInit();
-				switchEditors.go(current_id, options.mode);
-
-            	$(wrap).on( 'click', '.insert-media', function( event ) {
-					var elem = $( event.currentTarget ),
-						editor = elem.data('editor'),
-						options = {
-							frame:    'post',
-							state:    'insert',
-							title:    wp.media.view.l10n.addMedia,
-							multiple: true
-						};
-
-					event.preventDefault();
-
-					elem.blur();
-
-					if ( elem.hasClass( 'gallery' ) ) {
-						options.state = 'gallery';
-						options.title = wp.media.view.l10n.createGalleryTitle;
-					}
-
-					wp.media.editor.open( editor, options );
-				});
-			}
-		});
-	}
-})( jQuery, window )
+                $(wrap).on("click", ".insert-media", function(e) {
+                    var f = $(this), d = f.data("editor"), c = {frame: "post",state: "insert",title: wp.media.view.l10n.addMedia,multiple: true};
+                    e.preventDefault();
+                    f.blur();
+                    if (f.hasClass("gallery")) {
+                        c.state = "gallery";
+                        c.title = wp.media.view.l10n.createGalleryTitle
+                    }
+                    wp.media.editor.open(d, c);
+                });
+            }
+        });
+    }
+})( jQuery, window );
